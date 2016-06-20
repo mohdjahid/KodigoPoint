9cbc4c579fce6d4e74f5782cef24de71272e7d39
mohdjahid <mohd.jahid.jmi@gmail.com>
2016-06-20 18:26:14 +0530

    Editor
    Change in script.js file.....

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-        
+
+        function changeKeyword()
+        {
+            var str=document.getElementsByClassName("codetextarea");
+             for (i=0;i<str.length;i++)
+             {
+                var s=str[i].innerHTML;
+                var len=s.match( /"(.*?)"/g);
+
+                for(var j=0;j<len.length;j++)
+                { 
+                    var s1=len[j];
+                    var s2='<span style="color:#ff0080">'+len[j]+'</span>';
+                    s=s.replace(s1,s2);
+                }
                 
+
+                /*keyWords*/
+                var search=[/abstract/g,/assert/g,/boolean/g,/break/g,/byte /g,/case/g,/catch/g,/char/g,/class/g,/const/g,/continue/g,/default/g,/double/g,/do/g,/else/g,/enum/g,/extends/g,/final/g,/finally/g,/float/g,/for/g,/goto/g,/if/g,/implements/g,/import/g,/instanceof/g,/int /g,/interface/g,/long/g,/native/g,/new/g,/package/g,/private/g,/protected/g,/public/g,/return/g,/short/g,/static/g,/strictfp/g,/super/g,/switch/g,/synchronized/g,/this/g,/throw/g,/throws/g,/transient/g,/try/g,/void/g,/volatile/g,/while/g];
+                var keyword=['abstract','assert','boolean','break','byte ','case','catch','char','class','const','continue','default','double','do','else','enum','extends','final','finally','float','for','goto','if','implements','import','instanceof','int ','interface','long','native','new','package','private','protected','public','return','short','static','strictfp','super','switch','synchronized','this','throw','throws','transient','try','void','volatile','while'];
+
+                for(var k=0;k<search.length;k++)
+                    s=s.replace(search[k],'<span style="color:blue">'+keyword[k]+'</span>');
+
+                rep=s;
+                document.getElementsByClassName("codetextarea")[i].innerHTML=rep;
+            }
+        }
+
         function copyFunction(str)
         {
-            document.getElementById('cpy').setAttribute("title","copied");
             var ta=document.getElementById('ta');
             ta.style="visibility:show; position:absolute; z-index:2;";
             ta.innerText = document.getElementById(str).innerText;
@@ -52,22 +78,21 @@
         });
 
         
-            function getPos(){
-                // alert("hello");
-                var element=document.getElementById('top-of-page');
-                var viewportOffset = element.getBoundingClientRect();
-                var top=viewportOffset.top;
+        function getPos(){
+            var element=document.getElementById('top-of-page');
+            var viewportOffset = element.getBoundingClientRect();
+            var top=viewportOffset.top;
 
-                if(top < -100)
-                    document.getElementById("btn").className = "display-back-to-top-btn";
+            if(top < -100)
+                document.getElementById("btn").className = "display-back-to-top-btn";
 
-                else if(top==0)
-                    document.getElementById("btn").className = "back-to-top";
+            else if(top==0)
+                document.getElementById("btn").className = "back-to-top";
 
-                else if(top <-5 || top > 5 )
-                    document.getElementById("btn").className = "back-to-top";
+            else if(top <-5 || top > 5 )
+                document.getElementById("btn").className = "back-to-top";
 
-             }
+         }
 
 
             
