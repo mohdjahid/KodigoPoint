4a4a248f613caa518081a33e64eb47bd562bedec
mohdjahid <mohd.jahid.jmi@gmail.com>
2016-06-20 20:45:30 +0530

    KodigoPoint
    topic-pager is deleted

--- topic-pager.js
+++
@@ -1,3 +0,0 @@
-$('.pager').html("<li class='previous'><a href='#'><span class='glyphicon glyphicon-chevron-left'></span> Previous</a></li>"+
-	"<li class='active'><a href='#'><!--<span class='glyphicon'></span>--> Current</a></li>"+
-	"<li class='next'><a href='#'>Next <span class='glyphicon glyphicon-chevron-right'></span></a></li>");
